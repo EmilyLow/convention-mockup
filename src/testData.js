87bbd2ec626148c82a31ce3b707b9b17e5ea9512
EmilyLow <emlow@me.com>
2021-03-05 13:18:23 -0600

    Read me commit

---
+++ src/testData.js
@@ -0,0 +1,19 @@
+export default testData = {
+
+    settings: {
+        dayNum: 3,
+        hourNum: 10,
+        startHour: 9
+    },
+
+    events: [
+        {
+            name: "Talk 1"
+        }, 
+        {
+            name: "Talk 2"
+        }
+    ]
+
+}
+   
