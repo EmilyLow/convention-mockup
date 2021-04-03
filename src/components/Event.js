33f077b37ea3b5dc8f87acf994477ce762c99d1e
EmilyLow <emlow@me.com>
2021-04-03 16:34:29 -0500

    Added further details to test data

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Popover from '@material-ui/core/Popover';
+import React from "react";
 
 
 function Event({details}) {
@@ -14,12 +16,37 @@ function Event({details}) {
     // console.log("Start time: " + startTime);
     // console.log("Endtime: " + endTime);
     // console.log("Length: " + length);
-
+//     const [anchorEl, setAnchorEl] = React.useState(null);
+//     const divRef = React.useRef();
+//     const handleClick = (event) => {
+//     //   setAnchorEl(divRef.current);
+//     setAnchorEl(event.currentTarget);
+// }
+//     function handleClose() {
+//         setAnchorEl(null);
+//       }
   
+//       const open = Boolean(anchorEl);
+//     const id = open ? "simple-popover" : undefined;
 
     return(
         <EventStyle details={details} startTime = {startTime} endTime = {endTime} length = {length}>
             <Label>{details.name}</Label>
+            {/* <Popover 
+                 id={id}
+                 open={open}
+                 anchorEl={anchorEl}
+                 onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                    }}>               
+            The content of the Popover.
+            </Popover> */}
         </EventStyle>
     );
 }
