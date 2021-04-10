2438cdf3503fae9248e9d9a60f78dd2d95f3716f
EmilyLow <emlow@me.com>
2021-04-10 16:32:55 -0500

    Popover added to title

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,51 +2,57 @@ import styled from "styled-components";
 import Popover from '@material-ui/core/Popover';
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+    typography: {
+      padding: theme.spacing(2)
+    }
+  }));
+  
 
 function Event({details}) {
 
-    // console.log("Event created");
-    // console.log(details);
+
 
     let startTime = details.startTime.getHours() +(details.startTime.getMinutes() /60);
     let endTime = details.endTime.getHours() + (details.endTime.getMinutes() / 60);
     let length = endTime - startTime;
 
-    // console.log(details.name);
-    // console.log("Start time: " + startTime);
-    // console.log("Endtime: " + endTime);
-    // console.log("Length: " + length);
-//     const [anchorEl, setAnchorEl] = React.useState(null);
-//     const divRef = React.useRef();
-//     const handleClick = (event) => {
-//     //   setAnchorEl(divRef.current);
-//     setAnchorEl(event.currentTarget);
-// }
-//     function handleClose() {
-//         setAnchorEl(null);
-//       }
-  
-//       const open = Boolean(anchorEl);
-//     const id = open ? "simple-popover" : undefined;
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        console.log("click");
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? "simple-popover" : undefined;
 
     return(
         <EventStyle details={details} startTime = {startTime} endTime = {endTime} length = {length}>
-            <Label>{details.name}</Label>
-            {/* <Popover 
-                 id={id}
-                 open={open}
-                 anchorEl={anchorEl}
-                 onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                    }}>               
-            The content of the Popover.
-            </Popover> */}
+            <Label onClick={handleClick}>{details.name}</Label>
+            <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <p>{details.speaker}</p>
+          <button>Test</button>
+        </Popover>
         </EventStyle>
     );
 }
