5405a3b76d3c887644d810844500b8cf58ad7b97
EmilyLow <emlow@me.com>
2021-04-12 15:21:07 -0500

    Used grid to align items

--- src/components/ContentPane.js
+++ src/components/ContentPane.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
 import Popover from '@material-ui/core/Popover';
 import React from "react";
 import Card from '@material-ui/core/Card';
 import Divider from '@material-ui/core/Divider';
-
+import Typography from '@material-ui/core/Typography';
 import Location from '@material-ui/icons/LocationOn';
 import PeopleAlt from '@material-ui/icons/PeopleAlt';
 import Subject from '@material-ui/icons/Subject';
@@ -16,13 +17,26 @@ function ContentPane({details}) {
 
 
     return(<EventDiv>
-          <p>{details.name}</p>
-            <Divider light/>
-            { details.location != "" && <p><Location/>{details.location}</p>}
-            { details.speaker != "" && <p><PeopleAlt/>{details.speaker}</p>}
-            { details.summary != "" && <p><Subject/>{details.summary}</p>}
+        <Grid container>
+             <Grid item xs={12}> <Typography>{details.name}</Typography> </Grid>
+         
+             <Grid item xs={12}> <Divider light/> </Grid>
+
+     
+
+            { details.location != "" && <Grid item xs = {2}><Location/> </Grid>}
+            { details.location != "" && <Grid item xs = {10}><Typography>{details.location}</Typography></Grid>}
+
+            { details.speaker != "" && <Grid item xs = {2}><PeopleAlt/> </Grid>}
+            { details.speaker != "" && <Grid item xs = {10}><Typography>{details.speaker}</Typography></Grid>}
+
+            { details.summary != "" && <Grid item xs = {2}><Subject/> </Grid>}
+            { details.summary != "" && <Grid item xs = {10}><Typography>{details.summary}</Typography></Grid>}
             
+            {/* { details.speaker != "" && <Typography><PeopleAlt/> {details.speaker}</Typography>}
             
+            { details.summary != "" && <Typography><Subject/> {details.summary}</Typography>} */}
+        </Grid>
             {/* <button>Test</button> */}
     </EventDiv>)
 }
